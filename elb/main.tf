c5a8063fe1ac1961ca970c42ffd1559c53bbaa1c
praveen417 <praveen.417@rediffmail.com>
2020-01-16 11:50:12 +0530

    Load Balance

---
+++ elb/main.tf
@@ -0,0 +1,15 @@
+resource "aws_elb" "web-elb" {
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:8080/"
+  }
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "8080"
+    instance_protocol = "http"
+  }
+}
