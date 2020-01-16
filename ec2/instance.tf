00766ca4bda725dfc8c99a80dd7d1f39d72d0bce
praveen417 <praveen.417@rediffmail.com>
2020-01-16 14:57:47 +0530

    Update deploy File

--- ec2/instance.tf
+++ ec2/instance.tf
@@ -1,10 +1,22 @@
 provider "aws" {
-  region     = "${var.region}"
+  region = "${var.region}"
 }
-module "instance" {
-  source = "/home/pravpal/Desktop/learning/aws/module/instance"
-}
+#module "instance" {
+#  source = "/home/pravpal/Desktop/learning/aws/module/instance"
+#}
 
 #module "vpc" {
 #  source = "/home/pravpal/Desktop/learning/aws/module/vpc"
 #}
+#module "ECS" {
+#  source = "/home/pravpal/Desktop/learning/aws/module/ecs"
+#}
+#module "elb" {
+#  source = "/home/pravpal/Desktop/learning/aws/module/elb"
+#}
+module "bucket" {
+  source = "/home/pravpal/Desktop/learning/aws/module/bucket"
+}
+module "block-storage" {
+  source = "/home/pravpal/Desktop/learning/aws/module/block-storage"
+}
